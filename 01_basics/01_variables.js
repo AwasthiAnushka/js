e5a5f30df3e2ac99a8d6fa8dc8fde71c203d7b22
Anushka Awasthi <anushkaawasthi850@gmail.com>
2025-01-13 11:29:08 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144556
+let accountEmail = "roshni@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2 // not allowed not change
+accountEmail = "hgf@hg.com"
+accountPassword = "212121"
+accountCity = "Kanpur"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
