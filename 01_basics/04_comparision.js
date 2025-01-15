93c8aac41f91f980482857f4bb2f554ba9b44cf3
Anushka Awasthi <anushkaawasthi850@gmail.com>
2025-01-15 10:50:59 +0000

    datatypes done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === stickly check krta hai even datatype bhi
+
+console.log("2" === 2);
