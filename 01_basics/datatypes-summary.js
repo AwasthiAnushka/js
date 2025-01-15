effb6018d1b903e9720ed4265eef3f9ae9167ee9
Anushka Awasthi <anushkaawasthi850@gmail.com>
2025-01-15 11:08:24 +0000

    done

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,6 +1,7 @@
 //  Primitive
 
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+// js is dynamically typed language
 
 const score = 100
 const scoreValue = 100.3
@@ -18,13 +19,13 @@ console.log(id === anotherId);
 
 
 
-// Reference (Non primitive)
+// Reference type (Non primitive)
 
 // Array, Objects, Functions
 
 const heros = ["shaktiman", "naagraj", "doga"];
 let myObj = {
-    name: "hitesh",
+    name: "ritesh",
     age: 22,
 }
 
@@ -32,5 +33,31 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof anotherId);
+console.log(typeof anotherId);// tell about type of
+
+//+*********************************************************
+
+// stack (primitive), Heap (Non-primitive)
+
+let myname = "Hellohii"
+
+let anothername = "This"
+
+console.log(myname);
+console.log(anothername);
+
+let userOne = {
+    email : "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne 
+userTwo.email ="Rohit"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+
+
 
